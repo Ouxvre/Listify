7b12e0d4d470e8a8c2482660322f062721abab1f
Dendy Putra Pratama <ouxvre@gmail.com.com>
2025-04-30 09:44:20 +0700

    banyak perubahan

---
+++ public/main.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="sidebar">
+        <div class="logo">LISTIFY</div>
+
+        <hr class="garis-putih-sidebar"></hr>
+    
+        <nav class="nav-menu">
+          <span class="menu-title">- Main Menu</span>
+          <a href="#" class="nav-item"><i class="fas fa-home"></i> Home</a>
+          <a href="#" class="nav-item"><i class="fas fa-clock-rotate-left"></i> History</a>
+          <a href="#" class="nav-item"><i class="fas fa-box-archive"></i> Archive</a>
+        </nav>
+      </div>
+
+      <div class="main-content">
+        
+        <div class="add-task-section">
+            <h2>Add Task</h2>
+            <div class="task-inputs">
+              <input type="text" placeholder="Add new task">
+              <input type="text" placeholder="Task Title">
+              <input type="text" placeholder="Due Date">
+              <button class="add-btn">Add Task</button>
+            </div>
+          </div>
+          
+          <hr class="garis-putih"></hr>
+
+        <div class="task-section">
+            <div class="task-header">
+              <h2>My Task</h2>
+            </div>
+            <div class="task-container">
+              <!-- Tempat tugas akan ditampilkan -->
+            </div>
+          </div>
+
+          <button id="toggle-theme" class="theme-toggle">
+            <i class="fas fa-sun"></i>
+          </button>
+          
+      </div>
+
+      <div class="footer"></div>
+</body>
+<script src="js/main.js"></script>
+</html>
